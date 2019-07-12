59cc7cae1b1c0afc4624f58363e55f3027820e30
Umme Farhana <ummefarhana7246@gmail.com>
2019-07-12 19:56:26 +0600

    created Linked List

---
+++ Linked_List.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int input()
+{
+    int n;
+    cin >> n;
+    return n;
+}
+
+typedef struct Node node;
+
+struct Node {
+    int data;
+    node* nxt;
+};
+
+node* createList(node* root, int data)
+{
+    node* newNode = new node();
+
+    newNode -> data = data;
+    newNode -> nxt = NULL;
+
+    if (root == NULL)
+        return newNode;
+
+    root -> nxt = newNode;
+    return newNode;
+}
+
+int main()
+{
+    node* root = NULL, *ptr = NULL;
+
+    cout << "Number of inputs: ";
+    int n = input(), i;
+
+    for(i = 0; i < n; i++)
+    {
+        if(i == 1)
+        {
+            int data = input();
+            root = createList(root, data);
+            ptr = root;
+        }
+
+        else
+        {
+            int data = input();
+            ptr = createList(ptr, data);
+        }
+    }
+    ptr = root;
+
+    cout << "The list is: ";
+
+    while(ptr != NULL)
+    {
+        cout << " " << ptr -> data;
+        ptr = ptr -> nxt;
+    }
+
+    return 0;
+}
