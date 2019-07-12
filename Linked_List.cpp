e132d4a956a01a108fb16e707b29e9b96987630c
Umme Farhana <ummefarhana7246@gmail.com>
2019-07-13 02:18:38 +0600

    Traversing a linked list to find maximum, minimum, total & average value

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//taking inputs.
 int input()
 {
     int n;
@@ -15,6 +16,7 @@ struct Node {
     node* nxt;
 };
 
+//creating linked list
 node* createList(node* root, int data)
 {
     node* newNode = new node();
@@ -34,17 +36,21 @@ int main()
     node* root = NULL, *ptr = NULL;
 
     cout << "Number of inputs: ";
-    int n = input(), i;
+    int dataSet = input();
 
-    for(i = 1; i <= n; i++)
+    cout << "Enter the numbers: ";
+
+    for(int i = 0; i < dataSet; i++)
     {
-        if(i == 1)
+        // putting data to the root of the list
+        if(i == 0)
         {
             int data = input();
             root = createList(root, data);
             ptr = root;
         }
 
+        //putting data to the list except root
         else
         {
             int data = input();
@@ -53,13 +59,34 @@ int main()
     }
     ptr = root;
 
-    cout << "The list is: ";
+    int maxValue = root->data;
+    int minValue = root->data;
+    int total = 0;
+
+    cout << "\n\nThe list is:";
 
     while(ptr != NULL)
     {
         cout << " " << ptr -> data;
+
+        //Finding the maximum value
+        if(ptr->data >= maxValue) maxValue = ptr->data;
+
+        //Finding the minimum value
+        if(ptr->data <= minValue) minValue = ptr->data;
+
+        total = total + ptr->data;
+
         ptr = ptr -> nxt;
     }
+
+    //finding the average value
+    double average = double(total) / dataSet;
+
+    cout << "\nThe maximum value is " << maxValue;
+    cout << "\nThe minimum value is " << minValue;
+    cout << "\nTotal = " << total;
+    cout << "\nAverage = " << average << endl;
 
     return 0;
 }
